4c2588e1e837cc359adf9420cb82d6cdf62a6d62
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-08-15 20:44:09 +0600

    front page and other 2 page

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+		$(document).scroll(function (e) {
+		    var scrollTop = $(document).scrollTop();
+		    if (scrollTop > 0) {
+		        console.log(scrollTop);
+		        $('.main-header').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+		    } else {
+		        $('.main-header').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+		    }
+		});
